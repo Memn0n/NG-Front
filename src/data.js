d17b4168fd7c99f33a32d2463af67958d313c012
Memn0n <ngabantuu@gmail.com>
2023-05-02 17:06:34 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/gTd1qFH/My-project-1-7.png",
+        title: "CCTV",
+        desc: "GET THE BEST CCTV EQUIPMENT AND QUALITY INSTALLATION & MAINTENANCE SERVICES",
+        bg: "ffa500",
+        bg2: "https://i.ibb.co/DY5yQb4/Cam-3.jpg",
+        button: "CONTACT US",
+        img2: "https://i.ibb.co/vkbyr3J/cctv.png",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/qm5hpdh/gadgets.png",
+        title: "ELECTRONICS",
+        desc: "POWER YOUR TV, PC, OR WIFI ROUTER DURING LOAD-SHEDDING WITH QUALITY UPS BACKUPS RANGING FROM R499",
+        bg: "ffa550",
+        bg2: "https://i.ibb.co/qm5hpdh/gadgets.png",
+        button: "CONTACT US",
+        img2: "https://i.ibb.co/93RFVkP/My-project-1.png",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/JkRMJ3h/My-project-1-3.png",
+        title: "LIGHTS",
+        desc: "LOAD-SHEDDING SHOULD NOT KILL YOUR SPARK, LIGHT UP THE HOUSE WITH RECHARGABLE OR SOLAR LIGHTS",
+        bg: "ffa580",
+        bg2: "https://i.ibb.co/f8sS2XH/pic1.jpg",
+        button: "CONTACT US",
+        img2: "https://i.ibb.co/R2553vw/My-project-1-9.png",
+    }
+]
