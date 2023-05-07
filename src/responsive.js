051d47aca05fd3747aa83f03f2fec4654e5015d9
Memn0n <ngabantuu@gmail.com>
2023-05-07 16:24:33 +0200

    first commit

---
+++ src/responsive.js
@@ -0,0 +1,41 @@
+import {css} from "styled-components"
+
+export const hugetab = (props) => {
+    return css`
+        @media only screen and (max-width: 1100px) {
+            ${props}
+      }
+    `;
+};
+
+export const bigtab = (props) => {
+    return css`
+        @media only screen and (max-width: 1000px) {
+            ${props}
+      }
+    `;
+};
+
+export const tab = (props) => {
+    return css`
+        @media only screen and (max-width: 820px) {
+            ${props}
+      }
+    `;
+};
+
+export const mobile = (props) => {
+    return css`
+        @media only screen and (max-width: 620px) {
+            ${props}
+      }
+    `;
+};
+
+export const mobile2 = (props) => {
+    return css`
+        @media only screen and (max-width: 480px) {
+            ${props}
+      }
+    `;
+};
